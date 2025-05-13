96aceed469996ea9596cb0103ef5adcecc93f4fe
Joshua <jwhite83193@gmail.com>
2025-05-13 09:08:01 -0500

    Static To-Do List with React:

---
+++ Static To-Do List React/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import ToDoList from './components/ToDoList';
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header className="Header">
+        <Header />
+      </Header>
+
+      <div className="ToDoList">
+        <ToDoList />
+      </div>
+
+    </>
+  )
+}
+
+export default App;
---
+++ Static To-Do List React/src/components/ToDoList.jsx
@@ -0,0 +1,16 @@
+export default function ToDoList() {
+    
+    let tasks = ["Learn React", "Build a Project", "Read Documentation"];
+
+    return (
+
+        <div>
+            <ul>
+                <li>`${tasks[0]}`</li>
+                <li>`${tasks[1]}`</li>
+                <li>`${tasks[2]}`</li>
+            </ul>
+        </div>
+
+    );
+};
