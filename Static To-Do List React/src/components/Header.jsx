b8b658082811451dea44530098f00fd44dc788db
Joshua <jwhite83193@gmail.com>
2025-05-14 10:16:31 -0500

    Simple To-Do List with React: Styling Updated

--- Static To-Do List React/src/components/Header.jsx
+++ Static To-Do List React/src/components/Header.jsx
@@ -3,7 +3,9 @@ export default function Header() {
     return(
 
         <>
-            <header>My To-Do List</header>
+            <div class="head">
+                <header >My To-Do List</header>
+            </div>
         </>
 
     );
